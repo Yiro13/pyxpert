10f74c82898eacf56a2ce9c08df473a9510d9aa3
Yahir Leo <yahirrocki@outlook.com>
2025-02-04 00:09:57 -0600

    added backward chaining

--- pyxpert/main.py
+++ pyxpert/main.py
@@ -2,21 +2,33 @@ from rulesbased.knowledgeBase.rule import Rule
 from rulesbased.knowledgeBase.knowledgeBase import KnowledgeBase
 from rulesbased.inferenceEngine.inferenceEngine import InferenceEngine
 
-condition = {"Tos": True, "Fiebre": True, "Dolor de cabeza": True}
-rule = Rule(fact=condition, conclusion="Covid-19")
+condition = {"A": True, "B": True}
+rule = Rule(fact=condition, conclusion="C")
 
-condition1 = {"Covid-19": True, "Basado": False}
-rule1 = Rule(fact=condition1, conclusion="Influenza")
+condition1 = {"D": True, "E": True, "F": True}
+rule1 = Rule(fact=condition1, conclusion="G")
+
+condition2 = {"H": True, "I": True}
+rule2 = Rule(fact=condition2, conclusion="J")
+
+condition3 = {"C": True, "G": True}
+rule3 = Rule(fact=condition3, conclusion="K")
+
+condition4 = {"J": True, "G": True}
+rule4 = Rule(fact=condition4, conclusion="L")
+
+condition5 = {"K": True, "L": True}
+rule5 = Rule(fact=condition5, conclusion="M")
 
 kb = KnowledgeBase()
 
 kb.addRule(rule=rule)
 kb.addRule(rule=rule1)
-
-# print(kb.getKnowledgeBase())
+kb.addRule(rule=rule2)
+kb.addRule(rule=rule3)
+kb.addRule(rule=rule4)
+kb.addRule(rule=rule5)
 
 ie = InferenceEngine(knowledgeBase=kb)
 
-ie.genGraph()
-
-pepe = ie.getNodes()
+ie.forwardChain()
--- pyxpert/rulesbased/inferenceEngine/inferenceEngine.py
+++ pyxpert/rulesbased/inferenceEngine/inferenceEngine.py
@@ -1,12 +1,39 @@
+import networkx as nx
+import matplotlib.pyplot as plt
 from rulesbased.inferenceEngine.node import Node
 
 
 class InferenceEngine:
     def __init__(self, knowledgeBase) -> None:
         self.knowledgeBase = knowledgeBase
+        self.nodes = []
 
-    def getNodes(self):
-        return self.nodes
+    def backwardChain(self, evaluate: str) -> None:
+        self._genGraph()
+        self.printGraph()
+        node = self._getNode(evaluate)
+        if node is None:
+            print(f"No se encontró el nodo con el hecho '{evaluate}'.")
+            return
+        resultado = self._eval(node)
+        print(f"Resultado de la evaluación para '{evaluate}': {resultado}")
+
+    def _eval(self, node):
+        if node.status is not None:
+            return node.status
+
+        if not node.ant:
+            result = input(f"Ingrese el valor para '{node.fact}' (True/False): ")
+            node.status = result.strip().lower() in ["true", "1", "sí", "si"]
+            return node.status
+
+        results = []
+        for ant in node.ant:
+            result_ant = self._eval(ant)
+            results.append(result_ant)
+
+        node.status = all(results)
+        return node.status
 
     def _nodeExists(self, node: Node) -> bool:
         for nodes in self.nodes:
@@ -15,16 +42,43 @@ class InferenceEngine:
 
         return False
 
-    def genGraph(self) -> None:
-        self.nodes = []
+    def _getNode(self, fact: str) -> Node:
+        for node in self.nodes:
+            if node.fact == fact:
+                return node
+
+        return None
+
+    def _genGraph(self) -> None:
         for rule in self.knowledgeBase.rules:
-            conc = Node(fact=rule.conclusion)
+            conc = self._getNode(rule.conclusion)
+            if conc is None:
+                conc = Node(fact=rule.conclusion)
+                self.nodes.append(conc)
+
             for fact in rule.fact.keys():
-                node = Node(fact=fact)
+                node = self._getNode(fact)
+                if node is None:
+                    node = Node(fact=fact)
+                    self.nodes.append(node)
                 conc.ant.append(node)
                 node.next = conc
 
-                if not self._nodeExists(node=node):
-                    self.nodes.append(node)
+    def printGraph(self) -> None:
+        G = nx.DiGraph()
 
-            self.nodes.append(conc)
+        for node in self.nodes:
+            if node.ant:
+                for a in node.ant:
+                    G.add_edges_from([(a.fact, node.fact)])
+        plt.figure(figsize=(5, 5))
+        nx.draw(
+            G,
+            with_labels=True,
+            node_color="green",
+            edge_color="gray",
+            node_size=2000,
+            font_size=16,
+        )
+
+        plt.show()
